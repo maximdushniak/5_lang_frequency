e6634444484eb3dc146454d8a0430eef1b910b4c
maximdushniak <maxim.dushniak@gmail.com>
2016-09-30 19:38:28 +0300

    первое решение

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as myfile:
+        return myfile.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    mylist = re.sub('\W', ' ', text).split()
+    max_count = 0
+    max_cntword = ''
+    for i in mylist:
+        count = mylist.count(i)
+        if count>=max_count:
+            max_count = count
+            max_cntword = i
+    return max_cntword
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input(u'Имя файла [data.txt]: ')
+    if not filepath:
+        filepath = 'data.txt'
+    text = load_data(filepath)
+    print(get_most_frequent_words(text))
+
